d40685a0803e984b982701ef5ee80a01998e4a08
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-10-11 19:25:58 +0300

    6

--- js/modal-contacts.js
+++ js/modal-contacts.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-contacts-open]"),
+    closeModalBtn: document.querySelector("[data-modal-contacts-close]"),
+    modal: document.querySelector("[data-modal-contacts]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
--- js/modal-map.js
+++ js/modal-map.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-map-open]"),
     closeModalBtn: document.querySelector("[data-modal-map-close]"),
     modal: document.querySelector("[data-modal-map]"),
+    body:document.body,
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("overflow");
   }
 })();
